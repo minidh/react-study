d84bd5b63da34796061c50fc8aab25189cd29fbc
baqnngg <bbqdnrmas@gmaia.comn>
2024-04-08 21:14:23 +0900

    4_8

---
+++ 4_8/main.js
@@ -0,0 +1,46 @@
+const myName = "dohui"
+const email = "m_dohui59@naver.com"
+const hello = 'Hello ${myName}??'
+
+console.log(hello)
+
+const number = 10;
+const number2 = 10.55;
+
+const checked = True;
+const isShow = False;
+
+let undef;
+const obj = {abc:123};
+console.log(undef);
+console.log(obj.abc);
+console.log(obj.xyz);
+
+let name = null;
+console.log(name);
+name = "ihihi";
+console.log(name);
+
+const user = {
+    userName : "mdh",
+    age : 18,
+    isValid : true
+};
+console.log(user.userName);
+console.log(user.age);
+console.log(user.isValid);
+
+const fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits[0]);
+
+isShow = true;
+if (isShow) {
+    var aaa = "A"
+    console.log(aaa);
+}
+console.log(aaa);
+
+const a = 12;
+console.log(a);
+a = 999;
+console.log(a);
