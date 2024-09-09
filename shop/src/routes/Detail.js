08be2846ea0f746e07b99602a19e7bb02927276d
minidh <m_dohui59@naver.com>
2024-09-09 20:08:22 +0900

    9_9

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,3 +1,4 @@
+import { useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -11,10 +12,22 @@ let YelloBtn = styled.button`
     padding : 10px;
 `;
 function Detail (props) {
+    useEffect(() => {
+        console.log("안녕안녕");
+    });
+    let [count, setCount] = useState(0);
     let {id} = useParams();
     let myItem = props.shoes.find(function(x){
         return x.id == id
     });
+
+    let [alert,setAlert] = useState(true)
+    useEffect(() => {
+        let timer = setTimeout(() => {setAlert(false)},2000)
+        return () => {
+            clearTimeout(timer);
+        }
+    },[])
 
     return (
         <div className="container">
@@ -30,8 +43,9 @@ function Detail (props) {
                 </div>
                 <div>
                     <Box>
-                        <YelloBtn bg="orange">오렌지버튼</YelloBtn>
+                        <YelloBtn bg="orange" onClick={() => {setCount(count+1)}}>ii오렌지버튼</YelloBtn>
                         <YelloBtn bg="blue">파란버튼</YelloBtn>
+                        {alert == true ? <YelloBtn>2초후 사라짐</YelloBtn> : null}
                     </Box>
                 </div>
             </div>
