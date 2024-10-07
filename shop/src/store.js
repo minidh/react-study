f66b5bf9c6f82ac0c791525a049a48843f9f4ddd
minidh <m_dohui59@naver.com>
2024-10-07 20:17:33 +0900

    10_07

--- shop/src/store.js
+++ shop/src/store.js
@@ -1,5 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore,createSlice } from "@reduxjs/toolkit";
 
+let user = createSlice({
+    name : 'user',
+    initialState : 'kim',
+    reducers : {
+        chageName(state) {
+            return 'john' + state
+        }
+    }
+})
+export let {chageName} = user.actions
+
+let stock = createSlice({
+    name : 'stock',
+    initialState : [10,11,12]
+})
+let cart = createSlice({
+    name : 'cart',
+    initialState : [
+        {id : 0, name : 'white and black', count : 2},
+        {id : 2, name : 'Gray Yordan', count : 1}
+    ]
+})
 export default configureStore({
-    reducer : { }
+    reducer : {
+        user : user.reducer,
+        stock :stock.reducer,
+        cart : cart.reducer
+    }
 })
